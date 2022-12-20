bbdf1164d62e702c53dc48b329bcae142a492c69
Monica N <monica@softsuave.com>
2022-12-20 18:37:29 +0530

    Initial commit

---
+++ Bisleri/Bisleri.h
@@ -0,0 +1,18 @@
+//
+//  Bisleri.h
+//  Bisleri
+//
+//  Created by SOFTSUAVE on 20/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bisleri.
+FOUNDATION_EXPORT double BisleriVersionNumber;
+
+//! Project version string for Bisleri.
+FOUNDATION_EXPORT const unsigned char BisleriVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bisleri/PublicHeader.h>
+
+
